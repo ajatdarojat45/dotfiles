c5ae3e3421333dc5ab4eac547d835260bfd77348
Ajat Darojat <ajatdarojat45@gmail.com>
2022-10-02 11:47:36 +0700

    Update buffer_line.lua

--- config/home/.config/nvim/lua/buffer_line.lua
+++ config/home/.config/nvim/lua/buffer_line.lua
@@ -1,2 +1,13 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup{
+    options = {
+      indicator = {
+        style = 'underline',
+      },
+      diagnostics = 'coc',
+      diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+      end
+    },
+}

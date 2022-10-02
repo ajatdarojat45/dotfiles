62e7e842f0928b7de2178ab8d7752a0c429a8f2c
Ajat Darojat <ajatdarojat45@gmail.com>
2022-10-02 08:27:56 +0700

    Create override.lua

---
+++ config/home/.config/nvim/lua/override.lua
@@ -0,0 +1,16 @@
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+  view = {
+    side = "right",
+  },
+}

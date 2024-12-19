2ced84462d1f6b5f5c80210bfd1f6894f37fe7dc
Ajat Darojat <ajatdarojat45@gmail.com>
2024-12-19 15:03:52 +0700

    add nvim sh file

--- nvim_install.sh
+++ nvim_install.sh
@@ -1,18 +1,28 @@
+#!/bin/sh
+
 echo "Installing Dependencies"
 
-# Packages
+# Install Neovim
 brew install nvim
 
-# Install package manager - VimPlug
+# Install VimPlug
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Clone config
 echo "Cloning Config"
 git clone https://github.com/ajatdarojat45/dotfiles.git /tmp/dotfiles
 mv $HOME/.config/nvim $HOME/.config/nvim_backup
 mv /tmp/dotfiles/.config/nvim $HOME/.config/nvim
 rm -rf /tmp/dotfiles
 
-# Open Neovim and run PlugInstall
+# Install plugins
+echo "Installing Plugins"
 nvim -c 'PlugInstall' -c 'qa'
+
+# Install Language Servers using Mason
+echo "Installing Language Servers"
+nvim -c 'MasonInstall lua_ls tsserver html cssls jsonls eslint' -c 'qa'
+
+echo "Setup Complete!"
 

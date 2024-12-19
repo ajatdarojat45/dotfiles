5dbfc619f87a79f8d2a53868f7820238adec8595
Ajat Darojat <ajatdarojat45@gmail.com>
2024-12-19 14:28:11 +0700

    add nvim sh file

--- nvim_install.sh
+++ nvim_install.sh
@@ -1,8 +1,9 @@
 echo "Installing Dependencies"
+
 # Packages
 brew install nvim
 
-# Install package  manager - VimPlug
+# Install package manager - VimPlug
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -11,3 +12,7 @@ git clone https://github.com/ajatdarojat45/dotfiles.git /tmp/dotfiles
 mv $HOME/.config/nvim $HOME/.config/nvim_backup
 mv /tmp/dotfiles/.config/nvim $HOME/.config/nvim
 rm -rf /tmp/dotfiles
+
+# Open Neovim and run PlugInstall
+nvim -c 'PlugInstall' -c 'qa'
+

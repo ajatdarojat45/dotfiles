2ffd1f3b0ce2b6dcb15bd0ef78256bca02910795
Ajat Darojat <ajatdarojat45@gmail.com>
2025-01-07 05:17:09 +0700

    change margin and color

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Color Palette
-export BLACK=0xff181926
-export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
+# Color Palette (Tokyo Night Moon)
+export BLACK=0xff1a1b26
+export WHITE=0xffc0caf5
+export RED=0xfff7768e
+export GREEN=0xff9ece6a
+export BLUE=0xff7aa2f7
+export YELLOW=0xffe0af68
+export ORANGE=0xffff9e64
+export MAGENTA=0xffbb9af7
+export GREY=0xff565f89
 export TRANSPARENT=0x00000000
 
 # General bar colors
-export BAR_COLOR=0xa024273a
+export BAR_COLOR=0xa01f2335 # Semi-transparent background for bar
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0x903c3e4f
-export BACKGROUND_2=0x90494d64
+export BACKGROUND_1=0x901f2335 # Lighter semi-transparent background
+export BACKGROUND_2=0x90262a3f # Slightly darker semi-transparent background
 
-export POPUP_BACKGROUND_COLOR=0xff24273a
+export POPUP_BACKGROUND_COLOR=0xff1f2335 # Solid background for popups
 export POPUP_BORDER_COLOR=$WHITE
 
 export SHADOW_COLOR=$BLACK

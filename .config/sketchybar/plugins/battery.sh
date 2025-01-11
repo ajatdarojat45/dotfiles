4101ce2dcf462369d9d65c249e950334ff22b19f
Ajat Darojat <ajatdarojat45@gmail.com>
2025-01-11 10:42:34 +0700

    chore: update btop config, kitty and sketchybar

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -3,30 +3,64 @@
 source "$HOME/.config/sketchybar/icons.sh"
 source "$HOME/.config/sketchybar/colors.sh"
 
-PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-CHARGING=$(pmset -g batt | grep 'AC Power')
+BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+BATTERY_INFO="$(pmset -g batt)"
+PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 
 if [ $PERCENTAGE = "" ]; then
   exit 0
 fi
 
+#DRAWING=on
+#COLOR=$WHITE
+#case ${PERCENTAGE} in
+#  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
+#  ;;
+#  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
+#  ;;
+#  [3-5][0-9]) ICON=$BATTERY_50
+#  ;;
+#  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
+#  ;;
+#  *) ICON=$BATTERY_0; COLOR=$RED
+#esac
+#
+#if [[ $CHARGING != "" ]]; then
+#  ICON=$BATTERY_CHARGING
+#  DRAWING=off
+#fi
+#
+#sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
+
 DRAWING=on
 COLOR=$WHITE
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
-  ;;
-  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
-  ;;
-  [3-5][0-9]) ICON=$BATTERY_50
-  ;;
-  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
-  ;;
-  *) ICON=$BATTERY_0; COLOR=$RED
+9[0-9] | 100)
+	ICON=$BATTERY_100
+	COLOR=$GREEN
+	;;
+[6-8][0-9])
+	ICON=$BATTERY_75
+	COLOR=$GREEN
+	;;
+[3-5][0-9])
+	ICON=$BATTERY_50
+	COLOR=$GREEN
+	;;
+[1-2][0-9])
+	ICON=$BATTERY_25
+	COLOR=$ORANGE
+	;;
+*)
+	ICON=$BATTERY_0
+	COLOR=$RED
+	;;
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=$BATTERY_CHARGING
-  DRAWING=off
+	ICON=$BATTERY_CHARGING
+	# DRAWING=off
 fi
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
+sketchybar --set "$NAME" drawing="$DRAWING" icon="$ICON" icon.color="$COLOR" label="${BATT_PERCENT}%" label.color="$COLOR"

e7f95b927186b93ae5a5823daf45ec9d361a03fa
Ajat Darojat <ajatdarojat45@gmail.com>
2025-12-15 06:02:44 +0700

    fix: update LSP configuration for better support and add Go LSP settings

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,96 +1,89 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		---------------------------------------------------
-		-- Capabilities (nvim-cmp)
-		---------------------------------------------------
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    ---------------------------------------------------
+    -- Capabilities (nvim-cmp)
+    ---------------------------------------------------
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		---------------------------------------------------
-		-- Mason-LSPConfig
-		---------------------------------------------------
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"ts_ls", -- ✅ NEW (replaces tsserver)
-			},
-			automatic_installation = true,
-		})
+    ---------------------------------------------------
+    -- Mason-LSPConfig
+    ---------------------------------------------------
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "ts_ls", -- tsserver replacement
+        "gopls", -- 🔴 GO LSP (WAJIB)
+      },
+      automatic_installation = true,
+    })
 
-		---------------------------------------------------
-		-- Native Neovim 0.11+ LSP config (NO lspconfig.setup)
-		---------------------------------------------------
-		vim.lsp.config("lua_ls", {
-			capabilities = capabilities,
-		})
+    ---------------------------------------------------
+    -- Native Neovim 0.11+ LSP config
+    ---------------------------------------------------
 
-		vim.lsp.config("ts_ls", {
-			capabilities = capabilities,
-		})
+    -- Lua
+    vim.lsp.config("lua_ls", {
+      capabilities = capabilities,
+    })
 
-		---------------------------------------------------
-		-- LSP keymaps (buffer-local)
-		---------------------------------------------------
-		vim.api.nvim_create_autocmd("LspAttach", {
-			desc = "LSP actions",
-			callback = function(event)
-				local opts = { buffer = event.buf }
+    -- TypeScript
+    vim.lsp.config("ts_ls", {
+      capabilities = capabilities,
+    })
 
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-				vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+    -- Go (🔥 FIX WARNA GO)
+    vim.lsp.config("gopls", {
+      capabilities = capabilities,
+      settings = {
+        gopls = {
+          semanticTokens = true, -- 🔴 INI KUNCI UTAMA
+          analyses = {
+            unusedparams = true,
+            shadow = true,
+          },
+          staticcheck = true,
+        },
+      },
+    })
 
-				-- Hover (focus float)
-				vim.keymap.set("n", "ch", function()
-					vim.lsp.buf.hover()
-					vim.defer_fn(function()
-						for _, win in ipairs(vim.api.nvim_list_wins()) do
-							local cfg = vim.api.nvim_win_get_config(win)
-							if cfg.relative ~= "" then
-								vim.api.nvim_set_current_win(win)
-								break
-							end
-						end
-					end, 100)
-				end, opts)
+    ---------------------------------------------------
+    -- LSP keymaps (buffer-local)
+    ---------------------------------------------------
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "LSP actions",
+      callback = function(event)
+        local opts = { buffer = event.buf }
 
-				vim.keymap.set("n", "cr", vim.lsp.buf.rename, opts)
-				vim.keymap.set({ "n", "x" }, "cf", function()
-					vim.lsp.buf.format({ async = true })
-				end, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
 
-				vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "ch", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "cr", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "x" }, "cf", function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
+        vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "cd", vim.diagnostic.open_float, opts)
+      end,
+    })
 
-				-- Diagnostics (focus float)
-				vim.keymap.set("n", "cd", function()
-					vim.diagnostic.open_float()
-					vim.defer_fn(function()
-						for _, win in ipairs(vim.api.nvim_list_wins()) do
-							local cfg = vim.api.nvim_win_get_config(win)
-							if cfg.relative ~= "" then
-								vim.api.nvim_set_current_win(win)
-								break
-							end
-						end
-					end, 100)
-				end, opts)
-			end,
-		})
-
-		---------------------------------------------------
-		-- Global hover key
-		---------------------------------------------------
-		vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {
-			silent = true,
-			noremap = true,
-		})
-	end,
+    ---------------------------------------------------
+    -- Global hover key
+    ---------------------------------------------------
+    vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {
+      silent = true,
+      noremap = true,
+    })
+  end,
 }
+

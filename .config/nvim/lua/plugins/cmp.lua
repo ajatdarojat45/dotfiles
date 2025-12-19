8fc0ee732bd58c1229da43559bfc0693a4a9a41a
Ajat Darojat <ajatdarojat45@gmail.com>
2025-12-19 09:14:53 +0700

    feat: add Tab and Shift-Tab mappings for navigating completion items

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -24,6 +24,8 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
       },
       sources = {
         { name = "nvim_lsp" },
